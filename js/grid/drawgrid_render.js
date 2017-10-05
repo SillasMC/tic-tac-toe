c74ad62dc8965a6d9fa41a3b03dca1de9b321e3e
Sillas Camara <sillasmc@gmail.com>
2017-10-05 18:28:06 -0300

    Choose mark

---
+++ js/grid/drawgrid_render.js
@@ -0,0 +1,4 @@
+// Draw Grid for the game
+function drawGrid_Render (board) {
+	//content
+}
--- js/startgame/startgame_render.js
+++ js/startgame/startgame_render.js
@@ -1,12 +1,21 @@
 // Render the screen with the beginning of the game
-function startGame_Render() {
-	let board = $("#section-board-id");
+function startGame_Render(board) {
 
-	let paragraphQuestion = $("<p>").addClass("board-text")
-	.text("Choose your mark:");
+	let paragraphQuestion = $("<p>").addClass("board-top")
+							.text("Choose your mark:");
+
+	let linkX = $("<a>").addClass("choose-link")
+					.attr("id", "choose-link-x-id")
+					.text('x');
+
+	let linkO = $("<a>").addClass("choose-link")
+					.attr("id", "choose-link-o-id")
+					.text('o');
 
 	let paragraphChoice = $("<p>").addClass("board-text")
-	.html('<a class="choose-link">x</a> or <a class="choose-link">o</a>');
+							.append(linkX)
+							.append(' or ')
+							.append(linkO);
 
 	board.fadeOut("fast", function () {
 		board.empty().append(paragraphQuestion).append(paragraphChoice).fadeIn("slow");
