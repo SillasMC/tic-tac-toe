85bad34d981127d5185dfd1c1f72557d6cff9e2e
Sillas Camara <sillasmc@gmail.com>
2017-10-16 21:47:47 -0300

    Map victory patterns

--- js/startgame/match.js
+++ js/startgame/match.js
@@ -52,21 +52,79 @@ function defineComputerMark (playersChoice) {
 }
 
 function matchWinner () {
-	var grid = getGrid();
+	var grid		= getGrid();
+	var winnerSeq	= {
+		i_begin:	0,
+		j_begin:	0,
+		i_end:		0,
+		j_end:		0
+	};
 
+	// Transpose grid array
+	var gridTranpose = grid[0].map(function(col, i) {
+		return grid.map(function(row) {
+			return row[i];
+		});
+	});
+
+	var mainDiag	= '';
+	var secDiag		= '';
+
+	var aux = 2;
 	for (let i = 0; i < 3; i++) {
+		// Check for line win patterns
 		let line = grid[i].join('');
 		if (line === 'xxx' || line === 'ooo') {
+			winnerSeq.i_begin	= i;
+			winnerSeq.j_begin	= 0;
+			winnerSeq.i_end		= i;
+			winnerSeq.j_end		= 2;
+			// TODO Turn red the winner position
+
 			return grid[i][0];
 		}
-		//TODO Check for other victories
-		//for (let j = 0; j < 3; i++) {
 
-		//}
+		// Check for column win patterns
+		let column = gridTranpose[i].join('');
+		if (column === 'xxx' || column === 'ooo') {
+			winnerSeq.i_begin	= 0;
+			winnerSeq.j_begin	= i;
+			winnerSeq.i_end		= 2;
+			winnerSeq.j_end		= i;
+			// TODO Turn red the winner position
+
+			return grid[0][i];
+		}
+
+		mainDiag	+= grid[i][i];
+		secDiag		+= grid[i][aux];
+
+		aux			-= 1;
 	}
 
-	//TODO Check for draw game
+	// Check for main diagonal win pattern
+	if (mainDiag === 'xxx' || mainDiag === 'ooo') {
+		winnerSeq.i_begin	= 0;
+		winnerSeq.j_begin	= 0;
+		winnerSeq.i_end		= 2;
+		winnerSeq.j_end		= 2;
+		// TODO Turn red the winner position
 
+		return grid[0][0];
+	}
+
+	// Check for secondary diagonal win pattern
+	if (secDiag === 'xxx' || secDiag === 'ooo') {
+		winnerSeq.i_begin	= 0;
+		winnerSeq.j_begin	= 2;
+		winnerSeq.i_end		= 2;
+		winnerSeq.j_end		= 0;
+		// TODO Turn red the winner position
+
+		return grid[0][2];
+	}
+
+		//TODO Check for draw game
 
 	return;
 }
